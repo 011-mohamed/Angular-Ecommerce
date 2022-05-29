dc4eb017f92559c45244f5a839a14f79de0c8115
011-mohamed <salem.khalil.1998@gmail.com>
2022-05-29 20:01:53 +0100

    9th commit

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -77,12 +77,12 @@ export class ProductService {
   }
 
   addProductNyckel(formData: FormData){
-    const url = `https://www.nyckel.com/v1/functions/7aaigszss2ejx7t8/invoke`
+    
     return this.httpClient.post(url,formData);
   }
 
   searchProductNyckel(formData: FormData):Observable<any>{
-    const url = `https://www.nyckel.com/v0.9/functions/1wx5f24y974e1ntc/search`
+    
     return this.httpClient.post<any[]>(url,formData);
   }
 
@@ -102,9 +102,6 @@ export class ProductService {
   }
 
   getImageOfNyckelSearch(sampleId : string):Observable<ImageNyckel>{
-    const url = `https://www.nyckel.com/v1/functions/1wx5f24y974e1ntc/samples/${sampleId}`
-    const headers= new HttpHeaders()
-      .set('Authorization', 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6ImF0K2p3dCJ9.eyJuYmYiOjE2NTM4NDYzNTcsImV4cCI6MTY1Mzg0OTk1NywiaXNzIjoiaHR0cHM6Ly93d3cubnlja2VsLmNvbSIsImNsaWVudF9pZCI6ImFycHF1aGV4MXppczIwbnl4ZTk0ZGR4a2ViczRjemthIiwianRpIjoiRTM1NkU4ODZBRDBBNEU1QzFEM0MwM0M5MzRGM0JDN0IiLCJpYXQiOjE2NTM4NDYzNTcsInNjb3BlIjpbImFwaSJdfQ.kYNm7VyDOGVFdq483T-KZ5lzwT1xuy9GcUzjmnUVWmWt7dyqjEnunNXtxt6GDZnkNvT4X4uCEcD5A6DnCr2wBhlhopBjq8RLWBG_JCiJIrEXWWtPVNTU2xZX3rvMOCZNIa2z-zVcdOlRkIHGZJ22vw0fRjjB-15NumKX28twHRqBlHvtCsxRlStVUqN-ehZnYY0SqH-bWdKzfKvXO-ZlupI82fNJSx5OowXER9NNGgrLxScTVNBR6C6NoRIQsKW4Q2y_jGipx7-dih7Wf3GMc78VhTfz5YIQl-XA2sjTltnHt_BoHu_3KOkbtzIxMuKPanYD2Z9Eu5e3CHgMaZ24-Q')  
     return this.httpClient.get<ImageNyckel>(url,{'headers':headers}); 
   } 
   
